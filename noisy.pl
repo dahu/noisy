5f537d7b0cb3c769895b71d020265cd25ae6487b
dahu <barry.arthur@gmail.com>
2012-11-13 13:35:12 +0800

    initial release

---
+++ noisy.pl
@@ -0,0 +1,97 @@
+# Copyright (c) 2010 by Barry Arthur <barry.arthur@gmail.com>:
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# Use alsa's aplay to play a sound on channel activity, highlights and private
+# messages.
+#
+# History:
+# 2011-01-04, bairui <barry.arthur@gmail.com>:
+#     version 0.2: limited msg beeps to certain channels
+# 2010-07-03, bairui <barry.arthur@gmail.com>:
+#     version 0.1: initial release (modified from FlashCode's beep.pl)
+#
+
+use strict;
+
+my $version = "0.2";
+my $player = '/usr/bin/aplay';
+my $sound_dir = '~/home/chat/sounds';
+my %noisy_sounds = ('highlight'      => "$sound_dir/private_message.wav",
+                    'msg'            => "$sound_dir/channel_activity.wav",
+                    'pv'             => "$sound_dir/private_message.wav",
+                    'soft_highlight' => "$sound_dir/sonar.wav",
+                    'soft_msg'       => "$sound_dir/drip.wav",
+                    'soft_pv'        => "$sound_dir/drum.wav",
+                    'loud_highlight' => "$sound_dir/sonar.wav",
+                    'loud_msg'       => "$sound_dir/sonar.wav",
+                    'loud_pv'        => "$sound_dir/drum.wav",
+                    'norm_highlight' => "$sound_dir/sonar.wav",
+                    'norm_msg'       => "$sound_dir/channel_activity.wav",
+                    'norm_pv'        => "$sound_dir/drum.wav",
+                    'priv_highlight' => "$sound_dir/sonar.wav",
+                    'priv_msg'       => "$sound_dir/private_message.wav",
+                    'priv_pv'        => "$sound_dir/drum.wav",
+                  );
+
+# default values in setup file (~/.weechat/plugins.conf)
+my %default_noisy = ('highlight'         => "on",
+                     'pv'                => "on",
+                     'msg'               => "off",
+                     'msg_off_channels'  => "",
+                     'msg_soft_channels' => "",
+                     'msg_loud_channels' => "",
+                     'msg_norm_channels' => "",
+                     'msg_priv_channels' => ""
+                     );
+
+weechat::register("noisy", "bairui <barry.arthur\@gmail.com>", $version,
+                  "GPL3", "Play a sound on channel activity/highlight/private message", "", "");
+weechat::config_set_plugin("noisy_highlight", $default_noisy{'highlight'}) if (weechat::config_get_plugin("noisy_highlight") eq "");
+weechat::config_set_plugin("noisy_pv", $default_noisy{'pv'}) if (weechat::config_get_plugin("noisy_pv") eq "");
+weechat::config_set_plugin("noisy_msg", $default_noisy{'msg'}) if (weechat::config_get_plugin("noisy_msg") eq "");
+weechat::config_set_plugin("noisy_msg_channels", $default_noisy{'msg_off_channels'}) if (weechat::config_get_plugin("noisy_msg_off_channels") eq "");
+weechat::config_set_plugin("noisy_msg_soft_channels", $default_noisy{'msg_soft_channels'}) if (weechat::config_get_plugin("noisy_msg_soft_channels") eq "");
+weechat::config_set_plugin("noisy_msg_loud_channels", $default_noisy{'msg_loud_channels'}) if (weechat::config_get_plugin("noisy_msg_loud_channels") eq "");
+weechat::config_set_plugin("noisy_msg_norm_channels", $default_noisy{'msg_norm_channels'}) if (weechat::config_get_plugin("noisy_msg_norm_channels") eq "");
+weechat::config_set_plugin("noisy_msg_priv_channels", $default_noisy{'msg_priv_channels'}) if (weechat::config_get_plugin("noisy_msg_priv_channels") eq "");
+
+weechat::hook_signal("weechat_highlight", "noisy", "highlight");
+weechat::hook_signal("irc_pv", "noisy", "pv");
+weechat::hook_signal("freenode,irc_in_privmsg", "noisy", "msg");
+weechat::hook_signal('weechat_pv', 'noisy', "highlight");
+
+sub noisy {
+  my $action = $_[0];
+  my $server = $_[1];
+  my $message = $_[2]; # including #channel name
+  my $noisy = weechat::config_get_plugin("noisy_$action");
+  #weechat::print("", $message);
+  if ($message =~ weechat::config_get_plugin("noisy_msg_off_channels")) {
+    # do nothing
+  } elsif ($message =~ weechat::config_get_plugin("noisy_msg_soft_channels")) {
+    system("$player $noisy_sounds{'soft_'.$action} 2>/dev/null &") if ($noisy eq "on");
+  } elsif ($message =~ weechat::config_get_plugin("noisy_msg_loud_channels")) {
+    system("$player $noisy_sounds{'loud_'.$action} 2>/dev/null &") if ($noisy eq "on");
+  } elsif ($message =~ weechat::config_get_plugin("noisy_msg_norm_channels")) {
+    system("$player $noisy_sounds{'norm_'.$action} 2>/dev/null &") if ($noisy eq "on");
+  } elsif ($message =~ weechat::config_get_plugin("noisy_msg_priv_channels")) {
+    system("$player $noisy_sounds{'priv_'.$action} 2>/dev/null &") if ($noisy eq "on");
+  } elsif (($action =~ /(highlight|pv)/i) || ($message =~ weechat::config_get_plugin("noisy_msg_channels")) || ($noisy eq "on")) {
+    system("$player $noisy_sounds{$action} 2>/dev/null &");
+  }
+  return weechat::WEECHAT_RC_OK;
+}
